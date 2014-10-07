f450f48f020d3ddd2b6e17c64041f1652659e926
uesteibar <uesteibar@gmail.com>
2014-10-07 22:08:25 +0200

    working dice. Issue with icon.

---
+++ src/app.js
@@ -0,0 +1,48 @@
+var UI = require('ui');
+var Accel = require('ui/accel');
+var Vibe = require('ui/vibe');
+
+
+
+
+var main = new UI.Card({
+  title: 'PebbleDice',
+  icon: 'resources/pebbledice.png',
+  subtitle: 'Shake your hand to throw the dice!',
+  body: '',
+  style: 'large'
+});
+
+main.show();
+
+
+
+Accel.init();
+
+main.on('accelTap', function(e){
+  //calculate random number between 1 and 6
+  var result = Math.floor((Math.random() * 6) + 1);
+  console.log(result);
+
+  
+  
+  setTimeout(function() {
+    //vibrate to let the user know
+    Vibe.vibrate('short');
+  
+    //show the result
+    main.body('');
+    main.subtitle('Result:  ' + result.toString());
+    
+    setTimeout(function() {
+    main.body('Shake your hand to throw again!');
+  }, 1000);
+    
+  }, 500);
+  
+  
+  
+  
+  
+  
+});
